8812629000fba9b1a2bfdb0c513e306c5fad8638
shong <shong@edmunds.com>
2017-06-05 10:19:06 -0700

    Reimplemented using Longest Increasing Subsequence

--- 11456/11456.cc
+++ 11456/11456.cc
@@ -5,72 +5,42 @@
 
 using namespace std;
 
-vector<int> squashWeightsToIndices(const vector<int> &nums) {
-    unordered_map<int,int> numberToIndex;
-    vector<int> sorted(nums);
-    sort(sorted.begin(), sorted.end());
-    for (int i=0; i<sorted.size(); ++i) {
-        numberToIndex[sorted[i]] = i;
-    }
-    vector<int> squashed;
-    for (int num : nums) {
-        squashed.push_back(numberToIndex[num]);
-    }
-    return squashed;
-}
-
-vector<int> getMaxLengthFromIndexIncreasing(const vector<int> &indicies) {
-    int n = indicies.size();
-    vector<vector<int>> matrix = vector<vector<int>>(n, vector<int>(n, 0));
-    for (int row = n - 1; row >= 0; --row) {
-        for (int col = n - 1; col >= 0; --col) {
-            int toAdd = indicies[row] == col ? 1 : 0;
-            int down = (row != n - 1) ? matrix[row + 1][col] : 0;
-            int right = (col != n - 1) ? matrix[row][col + 1] : 0;
-            matrix[row][col] = max(down, right) + toAdd;
+vector<int> getLongestIncreasingSubsequences(const vector<int> &nums) {
+    vector<int> longestIncreasingSubsequences(nums.size(), 1);
+    for (int right = nums.size() - 1; right >= 0; --right) {
+        for (int left = right - 1; left >= 0; --left) {
+            if (nums[left] < nums[right]) {
+                longestIncreasingSubsequences[left] = max(longestIncreasingSubsequences[left], longestIncreasingSubsequences[right] + 1);
+            }
         }
     }
-    vector<int> toReturn;
-    for (int i=0; i<n; ++i) {
-        toReturn.push_back(matrix[i][indicies[i]]);
-    }
-    return toReturn;
+    return longestIncreasingSubsequences;
 }
 
-vector<int> getMaxLengthFromIndexDecreasing(const vector<int> &indicies) {
-    int n = indicies.size();
-    vector<vector<int>> matrix = vector<vector<int>>(n, vector<int>(n, 0));
-    for (int row = n - 1; row >= 0; --row) {
-        for (int col = 0; col < n; ++col) {
-            int toAdd = indicies[row] == col ? 1 : 0;
-            int down = (row != n - 1) ? matrix[row + 1][col] : 0;
-            int left = (col != 0) ? matrix[row][col - 1] : 0;
-            matrix[row][col] = max(down, left) + toAdd;
+vector<int> getLongestDecreasingSubsequences(const vector<int> &nums) {
+    vector<int> longestDecreasingSubsequences(nums.size(), 1);
+    for (int right = nums.size() - 1; right >= 0; --right) {
+        for (int left = right - 1; left >= 0; --left) {
+            if (nums[left] > nums[right]) {
+                longestDecreasingSubsequences[left] = max(longestDecreasingSubsequences[left], longestDecreasingSubsequences[right] + 1);
+            }
         }
     }
-    vector<int> toReturn;
-    for (int i=0; i<n; ++i) {
-        toReturn.push_back(matrix[i][indicies[i]]);
-    }
-    return toReturn;
-
+    return longestDecreasingSubsequences;
 }
 
 int findLongestTrain(const vector<int> &increasing, const vector<int> &decreasing) {
-    int maxLength = 0;
+    int longest = 0;
     for (int i=0; i<increasing.size(); ++i) {
-        int length = increasing[i] + decreasing[i] - 1;
-        maxLength = max(length, maxLength);
+        longest = max(longest, increasing[i] + decreasing[i] - 1);
     }
-    return maxLength;
-    return 0;
+    return longest;
 }
 
 int getLongestTrain(const vector<int> &weights) {
-    vector<int> indicies = squashWeightsToIndices(weights);
-    vector<int> maxLengthFromIndexIncreasing = getMaxLengthFromIndexIncreasing(indicies);
-    vector<int> maxLengthFromIndexDecreasing = getMaxLengthFromIndexDecreasing(indicies);
-    return findLongestTrain(maxLengthFromIndexIncreasing, maxLengthFromIndexDecreasing);
+    vector<int> longestIncreasingSubsequences = getLongestIncreasingSubsequences(weights);
+    vector<int> longestDecreasingSubsequences = getLongestDecreasingSubsequences(weights);
+    return findLongestTrain(longestIncreasingSubsequences, longestDecreasingSubsequences);
 }
 
 int main() {
